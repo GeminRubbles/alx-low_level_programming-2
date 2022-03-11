451447de7c50c4571701938b9d7428e5c7d38be2
GeminRubbles <99999756+GeminRubbles@users.noreply.github.com>
2022-03-11 03:27:39 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ *main - entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of
- * art,"
+ *Return: Always 0 (success)
  *
- * Return: Always 0 (Success)
  */
 int main(void)
 {
