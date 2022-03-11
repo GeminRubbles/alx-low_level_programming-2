42c587fc6e6c0746308e42e070f9590a03a06d0e
GeminRubbles <99999756+GeminRubbles@users.noreply.github.com>
2022-03-11 03:09:01 +0100

    Thanks Ayoola

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-nmmmm
-=======
 #include <stdio.h>
 
 /**
@@ -16,4 +13,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
->>>>>>> 6894cda9128f91390455210f94fb2f79ab0daa0e
