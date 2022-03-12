01c9218b9121f73c59bd53f97727d18339045272
GeminRubbles <99999756+GeminRubbles@users.noreply.github.com>
2022-03-12 05:59:33 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,25 +3,24 @@
 /**
  * main - print the letters of the alphabet
  *
- * Description: print thr letters of the alphaber except e, q
+ * Description: print the letters of the alphabet except e, q
  *
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	int i = 97;
-	
+
 	while (i < 123)
 	{
 		if (i != 101 && i != 113)
 		{
 			putchar(i);
 		}
-		i++:
+		i++;
 	}
 	putchar(10);
-	
-	return (0):
+
+	return (0);
 }
-		
