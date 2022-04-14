4dac415c89120e6f449a243dd5d210414fd548cb
GeminRubbles <99999756+GeminRubbles@users.noreply.github.com>
2022-04-14 21:55:34 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,25 @@
-#ifndef _VARIADIC_FUNCTIONS_
-#define _VARIADIC_FUNCTIONS_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
 #include <stdio.h>
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+
+/**
+ * struct fm - structure
+ * @fm: Pointer
+ * @p: pointer
+ */
+
+typedef struct fm
+{
+	char *fm;
+	void (*p)();
+} format_type;
+
 
 #endif
